ac829537c2ae83b12d7395bbf6edf9b87d57ae37
hlxwell <hlxwell@gmail.com>
2018-11-03 17:30:33 +0900

    change docker config

--- web_service/main.go
+++ web_service/main.go
@@ -1,12 +1,56 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"fmt"
+	"math/rand"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/kataras/iris"
+)
+
+type User struct {
+	gorm.Model
+	ID    int `gorm:"AUTO_INCREMENT"`
+	Email string
+}
+
+type Account struct {
+	gorm.Model
+	ID               int `gorm:"AUTO_INCREMENT"`
+	Currency         string
+	Balance          float64
+	AvailableBalance float64
+}
 
 func main() {
 	app := iris.Default()
 
+	// create db connection
+	db, err := gorm.Open("postgres", "host=127.0.0.1 port=5432 user=helixing password= dbname='go-exchange-engine' sslmode=disable")
+	defer db.Close()
+	// db.Model(user).Related(&account)
+
+	if err != nil {
+		fmt.Println(err)
+		panic("Failed to connect to database.")
+	}
+	db.AutoMigrate(&User{})
+
 	app.Handle("GET", "/", func(ctx iris.Context) {
-		ctx.HTML("Hello world!")
+		email := fmt.Sprintf("hlxwell%d@gmail.com", rand.Intn(100))
+		db.Create(&User{Email: email})
+
+		var users []User
+		db.Where("email = ?", "hlxwell1@gmail.com").Find(&users)
+		// db.Delete(users)
+
+		fmt.Println(email)
+		for _, user := range users {
+			fmt.Println(user.Email)
+		}
+
+		defer ctx.HTML("Hello world!")
 	})
 
 	app.Get("/ping", func(ctx iris.Context) {
@@ -17,5 +61,5 @@ func main() {
 		ctx.JSON(iris.Map{"message": "Hello iris web framework."})
 	})
 
-	app.Run(iris.Addr(":8080"))
+	app.Run(iris.Addr(":8000"))
 }
