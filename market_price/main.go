bd21b8efcd84c198d1896db48dcdd3b27247ddb1
hlxwell <hlxwell@gmail.com>
2018-11-11 23:01:17 +0900

    add basic code

---
+++ market_price/main.go
@@ -0,0 +1,7 @@
+package marketPrice
+
+import "fmt"
+
+func main() {
+	fmt.Println("market price")
+}
---
+++ sequencer/main.go
@@ -0,0 +1,7 @@
+package sequencer
+
+import "fmt"
+
+func main() {
+	fmt.Println("sequencer")
+}
