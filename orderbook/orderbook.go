e73bcdad9a04e56ef91153120728a23b5672a986
hlxwell <hlxwell@gmail.com>
2018-11-26 22:49:18 +0900

    moved the strike method to market

--- orderbook/orderbook.go
+++ orderbook/orderbook.go
@@ -15,95 +15,6 @@ type OrderBook struct {
 	MarketOrders  *rbt.Tree
 }
 
-// Strike the orderbook to check if any matching orders and generator trades
-//
-// Condition 1:
-// order => buy, ¥10.1
-// orderbook => sell 10.5, 11, 11.1
-// result => go to Buy orderbook
-//
-// Condition 2:
-// order => buy, ¥10.1
-// orderbook => sell 9.5, 10, 10.2
-// result => go to Buy orderbook
-//
-func (orderbook *OrderBook) Strike(myOrder *Order) *[]*Trade {
-	// Striking the wrong orderbook
-	// Buy should strike Selling orderbook, vice versa.
-	if orderbook.Side == myOrder.Side {
-		return nil
-	}
-
-	trades := &[]*Trade{}
-
-	if myOrder.Side == buy {
-		// buy order is scanning selling part from low -> high.
-		iter := orderbook.LimitedOrders.Iterator()
-		iter.Begin()
-		// loop the pricelevel
-		for iter.Next() {
-			priceLevel, _ := iter.Value().(*PriceLevel)
-			// loop the orders in pricelevel
-			for _, order := range *priceLevel.Orders {
-				if myOrder.Price >= priceLevel.Price && myOrder.Amount > 0 {
-					if myOrder.Amount > order.Amount {
-						// demand > supply
-						*trades = append(*trades, &Trade{Price: order.Price, Amount: order.Amount})
-						myOrder.Amount = myOrder.Amount - order.Amount
-						order.Amount = 0
-						// striked order should be deleted
-						// FIXME: Cannot delete order here.
-						// *priceLevel.Orders = append((*priceLevel.Orders)[:i], (*priceLevel.Orders)[i+1:]...)
-					} else if myOrder.Amount < order.Amount {
-						// supply > demand
-						*trades = append(*trades, &Trade{Price: order.Price, Amount: myOrder.Amount})
-						order.Amount = order.Amount - myOrder.Amount
-						myOrder.Amount = 0
-						break
-					}
-				} else if myOrder.Amount == 0 {
-					break
-				} else if myOrder.Price <= order.Price { // selling price is too high.
-					// we need to put the order to the buy orderbook.
-				}
-			}
-		}
-	} else if myOrder.Side == sell {
-		// sell order is scanning buying part from high -> low.
-		iter := orderbook.LimitedOrders.Iterator()
-		iter.End()
-		for iter.Prev() {
-			priceLevel, _ := iter.Value().(*PriceLevel)
-			// loop the orders in pricelevel
-			for _, order := range *priceLevel.Orders {
-				if myOrder.Price <= priceLevel.Price && myOrder.Amount > 0 {
-					if myOrder.Amount > order.Amount {
-						// demand > supply
-						*trades = append(*trades, &Trade{Price: order.Price, Amount: order.Amount})
-						myOrder.Amount = myOrder.Amount - order.Amount
-						order.Amount = 0
-						// striked order should be deleted
-						// FIXME: Cannot delete order here.
-						// *priceLevel.Orders = append((*priceLevel.Orders)[:i], (*priceLevel.Orders)[i+1:]...)
-					} else if myOrder.Amount < order.Amount {
-						// supply > demand
-						*trades = append(*trades, &Trade{Price: order.Price, Amount: myOrder.Amount})
-						order.Amount = order.Amount - myOrder.Amount
-						myOrder.Amount = 0
-						break
-					}
-				} else if myOrder.Amount == 0 {
-					break
-				} else if myOrder.Price <= order.Price { // selling price is too high.
-					// we need to put the order to the buy orderbook.
-				}
-			}
-		}
-	}
-
-	return trades
-}
-
 // GetPriceLevel get orders of a price level by price and order type
 func (orderbook *OrderBook) GetPriceLevel(price float64, orderType OrderType) (*PriceLevel, error) {
 	var iterator rbt.Iterator
@@ -201,8 +112,6 @@ func (orderbook *OrderBook) AddLimitedOrder(order *Order) {
 	orderbook.LimitedOrders.Put(priceLevel.Price, priceLevel)
 }
 
-// =====================================================================
-
 // AllLimitedOrders return all the limited orders
 func (orderbook *OrderBook) AllLimitedOrders() []*Order {
 	var orders []*Order
@@ -216,6 +125,8 @@ func (orderbook *OrderBook) AllLimitedOrders() []*Order {
 	}
 	return orders
 }
+
+// =====================================================================
 
 // CreateOrderBook : Create a new order book
 func CreateOrderBook(pair Pair, side Side) *OrderBook {
