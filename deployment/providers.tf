79471160bcf56862b74e7d69a1c8e6a11759f6ae
Michael He <michael.he@bytedance.com>
2021-06-07 00:17:26 +0900

    remove deployment

--- deployment/providers.tf
+++
@@ -1,6 +0,0 @@
-provider "google" {
-  credentials = "${file("~/.gcp/juju-gce-1-sa.json")}"
-  project     = "bitsx-vc-dev-poc"
-  zone        = "asia-northeast1-c"
-  region      = "asia-northeast1"
-}
--- deployment/variables.tf
+++
@@ -1,3 +0,0 @@
-variable "gcp_region" {
-  default = "asia-northeast1"
-}
